e946d9957b190f586ca42a2918129bd43043ede6
Cwalker924 <cwalker924@gmail.com>
2017-01-22 20:33:54 +0000

    updating

--- main.c
+++ main.c
@@ -13,5 +13,22 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
+	//if (FILE CANT OPEN )
+	//{
+	//	printf("Error: Can't open file %s\n", <FILENAME>);
+	//	exit(EXIT_FAILURE);
+	//}
 
+	//if (FILE HAS INVALID INSTRUCTIONS)
+	//{
+	//	printf("L%u<line_number>: unknown instruction %s<opcode>\n",
+	//	       line_num, opcode );
+	//	exit(EXIT_FAILURE);
+	//}
+
+	//if (CANT MALLOC MORE SPACE)
+	//{
+	//	printf("Error: malloc failed");
+	//	exit(EXIT_FAILURE);
+	//}
 }
