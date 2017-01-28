707e1466af57efee499f9feddcbdcc1da317d121
Cwalker924 <cwalker924@gmail.com>
2017-01-28 01:03:56 +0000

    updating changes

---
+++ monty/main_test.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char **argv)
+{
+	FILE *fp;
+	char *cmd, *sep_cmd, *file;
+	size_t length;
+	int gl, data;
+
+	file = argv[1];
+	fp = fopen(file, "r");
+
+	if (argc != 2)
+	{
+		printf("usage: monty file");
+		exit(EXIT_FAILURE);
+	}
+
+	gl = getline(&cmd, &length, fp);
+	while (gl >= 0)
+	{
+		sep_cmd = strtok(cmd, " \t\n");
+		printf("%s", sep_cmd);
+
+		sep_cmd = strtok(NULL, " \t\n");
+		data = atoi(sep_cmd);
+		printf("%d", data);
+
+	}
+	fclose(fp);
+	return(0);
+}
